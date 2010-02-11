ceff6c7729f098d95740c963d12d2e3bf8b552db
Timothy Bourke <tim@tbrk.org>
2010-02-11 07:49:47 +0100

    Initial import. * Working prototype. * Not yet extensively tested. * Translations not started.

---
+++ src/org/tbrk/mnemododo/ProgressTask.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2010 Timothy Bourke
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.tbrk.mnemododo;
+
+import mnemogogo.mobile.hexcsv.Progress;
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public abstract class ProgressTask<Params, Result>
+    extends UserTask<Params, Integer, Result>
+    implements Progress
+{
+    private ProgressDialog progress_dialog = null;
+    private int progress_max = 10000;
+    private int progress_level = 0;
+    protected int style = ProgressDialog.STYLE_SPINNER;
+
+    protected abstract String getMessage();
+    protected abstract Context getContext();
+
+    public void onProgressUpdate(Integer... progress)
+    {
+        if (progress[0] == -1) {
+            // do it here, and not in onPostExecute, because
+            // the update requests are queued.
+            if (progress_dialog != null) {
+                progress_dialog.dismiss();
+                progress_dialog = null;
+            }
+
+        } else {
+            if (progress_dialog == null) {
+                progress_dialog = new ProgressDialog(getContext());
+                progress_dialog.setProgressStyle(style);
+                progress_dialog.setMessage(getMessage());
+                progress_dialog.setMax(progress_max);
+                progress_dialog.setCancelable(false);
+                progress_dialog.show();
+            }
+            progress_dialog.setProgress(progress[0]);
+        }
+    }
+
+    public void startOperation(int length, String msg)
+    {
+        progress_level = 0;
+        progress_max = length;
+        publishProgress(0);
+    }
+
+    public void updateOperation(int delta)
+    {
+        progress_level += delta;
+        publishProgress(progress_level);
+    }
+
+    public void stopOperation()
+    {
+        progress_level = 10000;
+        progress_max = 10000;
+        publishProgress(-1);
+    }
+}
---
+++ src/org/tbrk/mnemododo/Settings.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2010 Timothy Bourke
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.tbrk.mnemododo;
+
+import java.util.Vector;
+
+import mnemogogo.mobile.hexcsv.FindCardDirAndroid;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+
+public class Settings
+    extends PreferenceActivity
+{
+    private class FindCardDirsTask extends
+            UserTask<ListPreference, Integer, Vector<String>>
+    {
+        private ListPreference list_pref = null;
+        private ProgressDialog progress_dialog;
+
+        public void onPreExecute()
+        {
+            progress_dialog = ProgressDialog.show(Settings.this, "",
+                    getString(R.string.searching_for_card_dirs), true);
+        }
+
+        public Vector<String> doInBackground(ListPreference... pref)
+        {
+            if (pref.length == 0) {
+                list_pref = null;
+                return null;
+            }
+
+            list_pref = pref[0];
+            return FindCardDirAndroid.list();
+        }
+
+        public void onPostExecute(Vector<String> result)
+        {
+            progress_dialog.dismiss();
+
+            if (list_pref == null) {
+                return;
+            }
+
+            if (result.isEmpty()) {
+                list_pref.setSummary(R.string.cannot_find_card_dirs);
+
+            } else {
+                String[] dirs = result.toArray(new String[result.size()]);
+                list_pref.setEntries(dirs);
+                list_pref.setEntryValues(dirs);
+
+                list_pref.setEnabled(true);
+            }
+        }
+    }
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+
+        ListPreference pref_card_dir = (ListPreference) getPreferenceScreen()
+                .findPreference("cards_path");
+        CharSequence[] entries = pref_card_dir.getEntries();
+
+        if ((entries == null) || (entries.length == 0)) {
+            pref_card_dir.setEnabled(false);
+            new FindCardDirsTask().execute(pref_card_dir);
+        }
+
+        setResult(RESULT_OK);
+    }
+
+    public void onContentChanged()
+    {
+        super.onContentChanged();
+    }
+}
